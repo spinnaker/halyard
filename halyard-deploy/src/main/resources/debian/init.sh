80ad51c6537250651882518040406fd98f37b194
Brandon Nelson <brnelson@google.com>
2017-11-28 11:17:25 -0500

    feat(xenial_builds): Add parent service for all spinnaker services.

--- halyard-deploy/src/main/resources/debian/init.sh
+++ halyard-deploy/src/main/resources/debian/init.sh
@@ -1,27 +1,23 @@
 source /etc/os-release
 
 if [ "$VERSION_ID" = "14.04" ]; then
-	cat > /etc/init/spinnaker.conf <<EOL
+  cat > /etc/init/spinnaker.conf <<EOL
 description "spinnaker"
-
 start on filesystem or runlevel [2345]
 stop on shutdown
-
 pre-start script
-
   for i in {%services%}
   do
     if [ ! -d "/var/log/spinnaker/\$i" ]; then
       echo "/var/log/spinnaker/\$i does not exist. Creating it..."
       install --mode=755 --owner=spinnaker --group=spinnaker --directory /var/log/spinnaker/\$i
     fi
-
     service \$i start
   done
 end script
 EOL
 elif [ "$VERSION_ID" = "16.04" ]; then
-	cat > /lib/systemd/system/spinnaker.target <<EOL
+  cat > /lib/systemd/system/spinnaker.service <<EOL
 [Unit]
 Description=All Spinnaker services
 After=network.target
