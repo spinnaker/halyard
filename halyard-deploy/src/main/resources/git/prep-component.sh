spinnaker/spinnaker

commit 244e875abf9cd1c290769551c183880e74b7323d

fix(localgit): Remove and then add git remote (#818)
If we run `git remote add upstream` when `upstream` already
exists, we get the annoying output `fatal: remote upstream already exists.`
for every component.

Run `git remote remove upstream` first to avoid printing this error.
This also mimics the behavior of the newer git command `git remote
set-url` that we use when git is above a certain version.

halyard-deploy/src/main/resources/git/prep-component.sh
@@ -29,7 +29,7 @@ git remote get-url upstream 2> /dev/null
 ERR=$?
 
 if [ "$ERR" -ne 0 ]; then
-  git remote add upstream $UPSTREAM_URL
+  git remote remove upstream && git remote add upstream $UPSTREAM_URL
 else
   git remote set-url upstream $UPSTREAM_URL
 fi

spinnaker/halyard

commit 8ad8ee6d961ca0609d3d8f8836fea456d9578904

fix(localgit): Do not fail if removing git remote fails
The remote "upstream" might not exist. Do not fail if so.

fixes bug introduced in https://github.com/spinnaker/halyard/pull/818

halyard-deploy/src/main/resources/git/prep-component.sh
@@ -29,7 +29,8 @@ git remote get-url upstream 2> /dev/null
 ERR=$?
 
 if [ "$ERR" -ne 0 ]; then
-  git remote remove upstream && git remote add upstream $UPSTREAM_URL
+  git remote remove upstream 2> /dev/null
+  git remote add upstream $UPSTREAM_URL
 else
   git remote set-url upstream $UPSTREAM_URL
 fi