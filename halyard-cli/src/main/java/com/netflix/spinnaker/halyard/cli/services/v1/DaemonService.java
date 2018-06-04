add968f9919dc52fdc68b3194a8e3e11db2f2ed0
Joon Lim <joonlim@google.com>
2018-06-04 14:28:33 -0400

    fix(version/bom) Add comment explaining why getBillOfMaterialsV2 should be used.

--- halyard-cli/src/main/java/com/netflix/spinnaker/halyard/cli/services/v1/DaemonService.java
+++ halyard-cli/src/main/java/com/netflix/spinnaker/halyard/cli/services/v1/DaemonService.java
@@ -724,9 +724,14 @@ public interface DaemonService {
   @GET("/v1/versions/latest/")
   DaemonTask<Halconfig, String> getLatest();
 
+  @Deprecated
   @GET("/v1/versions/bom/{version}")
   DaemonTask<Halconfig, Object> getBillOfMaterials(@Path("version") String version);
 
+  /**
+   * This method should be used in place of getBillOfMaterials because of Spring's inability to
+   * receive encoded forward slashes in a path variable.
+   */
   @GET("/v1/versions/bom")
   DaemonTask<Halconfig, Object> getBillOfMaterialsV2(@Query("version") String version);
 
