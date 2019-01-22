d52377e374936374db67656545ba1028638bb9d0
Zhiqing Qiu <zhqqi@microsoft.com>
2019-01-22 10:35:52 +0800

    Remove unused class definition

--- halyard-config/src/main/java/com/netflix/spinnaker/halyard/config/model/v1/providers/azure/AzureProvider.java
+++ halyard-config/src/main/java/com/netflix/spinnaker/halyard/config/model/v1/providers/azure/AzureProvider.java
@@ -20,11 +20,6 @@ import com.netflix.spinnaker.halyard.config.model.v1.node.HasImageProvider;
 import lombok.Data;
 
 public class AzureProvider extends HasImageProvider<AzureAccount, AzureBakeryDefaults> {
-//  @Data
-//  public static class AzureRegion {
-//    String name;
-//  }
-
   @Override
   public ProviderType providerType() {
     return ProviderType.AZURE;
