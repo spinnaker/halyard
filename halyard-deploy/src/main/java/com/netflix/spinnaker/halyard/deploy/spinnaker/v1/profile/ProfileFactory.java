d2e9bf800847392355a9aef510ff166bc66c3c34
Nicolas Cohen <nicolas.cohen@armory.io>
2019-01-30 12:45:45 -0800

    Remove extraneous line

--- halyard-deploy/src/main/java/com/netflix/spinnaker/halyard/deploy/spinnaker/v1/profile/ProfileFactory.java
+++ halyard-deploy/src/main/java/com/netflix/spinnaker/halyard/deploy/spinnaker/v1/profile/ProfileFactory.java
@@ -129,6 +129,5 @@ abstract public class ProfileFactory {
               halconfigDirectoryStructure.getStagingDependenciesPath(deploymentName));
     }
     return mapper.convertValue(o, Map.class);
-
   }
 }
