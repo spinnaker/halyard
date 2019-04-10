ef5ee7bcba669368875f43a37077f5026b22b2c4
Kathryn Lewis <kathryn.lewis@armory.io>
2019-04-10 14:30:58 -0700

    fix(clouddriver/profile): re-parentify deployment configuration after cloning providers

--- halyard-deploy/src/main/java/com/netflix/spinnaker/halyard/deploy/spinnaker/v1/profile/ClouddriverProfileFactory.java
+++ halyard-deploy/src/main/java/com/netflix/spinnaker/halyard/deploy/spinnaker/v1/profile/ClouddriverProfileFactory.java
@@ -83,6 +83,7 @@ public class ClouddriverProfileFactory extends SpringProfileFactory {
         .setRequiredFiles(files);
 
     deploymentConfiguration.setProviders(originalProviders);
+    deploymentConfiguration.parentify();
   }
 
   protected void processProviders(Providers providers) {
