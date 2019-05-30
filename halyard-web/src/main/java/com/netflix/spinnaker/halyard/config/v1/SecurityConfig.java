7c86b75960043f6fc8cc6924d638ba89f38f025b
Pierre Delagrave <pdelagrave@pivotal.io>
2019-05-29 21:49:21 -0400

    Disabled csrf for halyard-web endpoints, moved the SecurityConfig class to halyard-web where it belong.

--- halyard-web/src/main/java/com/netflix/spinnaker/halyard/config/v1/SecurityConfig.java
+++ halyard-web/src/main/java/com/netflix/spinnaker/halyard/config/v1/SecurityConfig.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.netflix.spinnaker.halyard.config.config.v1;
+package com.netflix.spinnaker.halyard.config.v1;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -25,6 +25,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   public void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests().anyRequest().permitAll();
+    http.csrf().disable().authorizeRequests().anyRequest().permitAll();
   }
 }
