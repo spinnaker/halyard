80ddbf98ab318d26b238fc41b34e5bd6c168e6bf
clane <clane@salesforce.com>
2019-07-11 03:14:26 -0400

    Fix typo

--- halyard-deploy/src/main/java/com/netflix/spinnaker/halyard/deploy/spinnaker/v1/service/DeploymentStrategy.java
+++ halyard-deploy/src/main/java/com/netflix/spinnaker/halyard/deploy/spinnaker/v1/service/DeploymentStrategy.java
@@ -6,10 +6,11 @@ import lombok.Data;
 
 @Data
 public class DeploymentStrategy {
-    DeploymentStrategy.Type type = Type.rollingUpdate;
-    Map<String, String> strategySettings = new HashMap<>();
-    public enum Type {
-        rollingUpdate,
-        recreate
-    }
+  DeploymentStrategy.Type type = Type.rollingUpdate;
+  Map<String, String> strategySettings = new HashMap<>();
+
+  public enum Type {
+    rollingUpdate,
+    recreate
+  }
 }
