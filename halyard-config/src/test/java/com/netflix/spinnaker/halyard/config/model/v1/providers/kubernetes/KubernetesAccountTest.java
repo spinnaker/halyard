fe13decc7e4aabb99ea9868373af59d05009a9a2
Michael Plump <plumpy@google.com>
2019-07-24 11:04:41 -0400

    fix(config): parse 'oauthScopes' stanzas that were incorrectly written
    The old version of Lombok used in Halyard <=1.21 didn't copy
    @JsonProperty annotations to the generated methods. This means Jackson
    was seeing the field as 'oAuthScopes' and the getter/setter as a
    separate 'oauthScopes' property. The same data would be written to both
    properties, and during parsing whichever came last in the file would be
    persisted into the object.
    
    With new versions of Lombok (>=1.18.8), the @JsonProperty annotation is
    copied to the bean methods, so the 'oauthScopes' property disappears and
    Jackson can no longer parse those old files.
    
    This commit adds some methods to support parsing the files generated by
    previous versions of Halyard, but will no longer write out the
    incorrect duplicate data.

---
+++ halyard-config/src/test/java/com/netflix/spinnaker/halyard/config/model/v1/providers/kubernetes/KubernetesAccountTest.java
@@ -0,0 +1,97 @@
+package com.netflix.spinnaker.halyard.config.model.v1.providers.kubernetes;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableList;
+import com.netflix.spinnaker.halyard.config.config.v1.StrictObjectMapper;
+import java.io.IOException;
+import java.io.StringWriter;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.yaml.snakeyaml.Yaml;
+
+@RunWith(JUnitPlatform.class)
+final class KubernetesAccountTest {
+
+  @Test
+  void testLastOAuthScopeIsKept_capitalA() {
+    Yaml yamlParser = new Yaml();
+    Object parsedYaml =
+        yamlParser.load(
+            Joiner.on('\n')
+                .join(
+                    "oauthScopes: [\"lowercase-a\"]", //
+                    "oAuthScopes: [\"uppercase-a\"]"));
+    KubernetesAccount account =
+        new StrictObjectMapper().convertValue(parsedYaml, KubernetesAccount.class);
+
+    assertThat(account.getOAuthScopes()).containsExactly("uppercase-a");
+  }
+
+  @Test
+  void testLastOAuthScopeIsKept_lowercaseA() {
+    Yaml yamlParser = new Yaml();
+    Object parsedYaml =
+        yamlParser.load(
+            Joiner.on('\n')
+                .join(
+                    "oAuthScopes: [\"uppercase-a\"]", //
+                    "oauthScopes: [\"lowercase-a\"]"));
+    KubernetesAccount account =
+        new StrictObjectMapper().convertValue(parsedYaml, KubernetesAccount.class);
+
+    assertThat(account.getOAuthScopes()).containsExactly("lowercase-a");
+  }
+
+  @Test
+  void testLastOAuthServiceAccountIsKept_capitalA() {
+    Yaml yamlParser = new Yaml();
+    Object parsedYaml =
+        yamlParser.load(
+            Joiner.on('\n')
+                .join(
+                    "oauthServiceAccount: \"lowercase-a\"", //
+                    "oAuthServiceAccount: \"uppercase-a\""));
+    KubernetesAccount account =
+        new StrictObjectMapper().convertValue(parsedYaml, KubernetesAccount.class);
+
+    assertThat(account.getOAuthServiceAccount()).isEqualTo("uppercase-a");
+  }
+
+  @Test
+  void testLastOAuthServiceAccountIsKept_lowercaseA() {
+    Yaml yamlParser = new Yaml();
+    Object parsedYaml =
+        yamlParser.load(
+            Joiner.on('\n')
+                .join(
+                    "oAuthServiceAccount: \"uppercase-a\"", //
+                    "oauthServiceAccount: \"lowercase-a\""));
+    KubernetesAccount account =
+        new StrictObjectMapper().convertValue(parsedYaml, KubernetesAccount.class);
+
+    assertThat(account.getOAuthServiceAccount()).isEqualTo("lowercase-a");
+  }
+
+  @Test
+  void testOnlyCapitalAIsWritten() throws IOException {
+    KubernetesAccount account = new KubernetesAccount();
+    account.setOAuthScopes(ImmutableList.of("my-scope"));
+    account.setOAuthServiceAccount("my-service-account");
+
+    String result = getYaml(account);
+
+    assertThat(result).contains("oAuthScopes");
+    assertThat(result).doesNotContain("oauthScopes");
+    assertThat(result).contains("oAuthServiceAccount");
+    assertThat(result).doesNotContain("oauthServiceAccount");
+  }
+
+  private static String getYaml(KubernetesAccount account) throws IOException {
+    StringWriter stringWriter = new StringWriter();
+    new StrictObjectMapper().writeValue(stringWriter, account);
+    return stringWriter.toString();
+  }
+}
