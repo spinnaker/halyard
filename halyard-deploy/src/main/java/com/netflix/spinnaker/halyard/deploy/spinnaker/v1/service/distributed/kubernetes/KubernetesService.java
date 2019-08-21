13344f1cd3ede49d4cc3170f14c9c99125ec96c1
Pierre Delagrave <pdelagrave@pivotal.io>
2019-08-21 10:19:41 -0400

    refactor(deploy/kubernetes): Removed duplicated code

---
+++ halyard-deploy/src/main/java/com/netflix/spinnaker/halyard/deploy/spinnaker/v1/service/distributed/kubernetes/KubernetesService.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2019 Pivotal, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.halyard.deploy.spinnaker.v1.service.distributed.kubernetes;
+
+import com.netflix.spinnaker.clouddriver.kubernetes.v1.deploy.KubernetesUtil;
+import com.netflix.spinnaker.clouddriver.kubernetes.v1.deploy.description.servergroup.KubernetesImageDescription;
+import com.netflix.spinnaker.halyard.config.model.v1.node.DeploymentConfiguration;
+import com.netflix.spinnaker.halyard.core.registry.v1.Versions;
+import com.netflix.spinnaker.halyard.deploy.services.v1.ArtifactService;
+import com.netflix.spinnaker.halyard.deploy.spinnaker.v1.SpinnakerArtifact;
+
+public interface KubernetesService {
+  String getDockerRegistry(String deploymentName, SpinnakerArtifact artifact);
+
+  SpinnakerArtifact getArtifact();
+
+  ArtifactService getArtifactService();
+
+  default String getArtifactId(DeploymentConfiguration deploymentConfiguration) {
+    String deploymentName = deploymentConfiguration.getName();
+    String artifactName = getArtifact().getName();
+    String version = getArtifactService().getArtifactVersion(deploymentName, getArtifact());
+    version = Versions.isLocal(version) ? Versions.fromLocal(version) : version;
+
+    KubernetesImageDescription image =
+        KubernetesImageDescription.builder()
+            .registry(getDockerRegistry(deploymentName, getArtifact()))
+            .repository(artifactName)
+            .tag(version)
+            .build();
+    return KubernetesUtil.getImageId(image);
+  }
+}
