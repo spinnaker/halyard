spinnaker/spinnaker

commit e7f99b19f6b3fe3fc2d2f15b7f32713b4ad86ff3

feat(notifications): Add Github Status support (#1374)

halyard-config/src/main/java/com/netflix/spinnaker/halyard/config/model/v1/node/Notification.java
@@ -43,7 +43,8 @@ public abstract class Notification extends Node implements Cloneable {
 
   public enum NotificationType {
     SLACK("slack"),
-    TWILIO("twilio");
+    TWILIO("twilio"),
+    GITHUB_STATUS("github-status");
 
     private final String name;
 

halyard-config/src/main/java/com/netflix/spinnaker/halyard/config/model/v1/node/Notifications.java
@@ -19,11 +19,10 @@
 package com.netflix.spinnaker.halyard.config.model.v1.node;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.netflix.spinnaker.halyard.config.model.v1.notifications.GithubStatusNotification;
 import com.netflix.spinnaker.halyard.config.model.v1.notifications.SlackNotification;
 import com.netflix.spinnaker.halyard.config.model.v1.notifications.TwilioNotification;
-import java.lang.reflect.Field;
-import java.util.Arrays;
-import java.util.Optional;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -33,6 +32,9 @@ public class Notifications extends Node implements Cloneable {
   SlackNotification slack = new SlackNotification();
   TwilioNotification twilio = new TwilioNotification();
 
+  @JsonProperty("github-status")
+  GithubStatusNotification githubStatus = new GithubStatusNotification();
+
   @Override
   public String getNodeName() {
     return "notification";
@@ -40,21 +42,21 @@ public class Notifications extends Node implements Cloneable {
 
   @JsonIgnore
   public boolean isEnabled() {
-    return slack.isEnabled() || twilio.isEnabled();
+    return slack.isEnabled() || twilio.isEnabled() || githubStatus.isEnabled();
   }
 
   public static Class<? extends Notification> translateNotificationType(String notificationName) {
-    Optional<? extends Class<?>> res =
-        Arrays.stream(Notifications.class.getDeclaredFields())
-            .filter(f -> f.getName().equals(notificationName))
-            .map(Field::getType)
-            .findFirst();
-
-    if (res.isPresent()) {
-      return (Class<? extends Notification>) res.get();
-    } else {
-      throw new IllegalArgumentException(
-          "No notification type with name \"" + notificationName + "\" handled by halyard");
+    Class<? extends Notification> n;
+    switch (Notification.NotificationType.valueOf(notificationName)) {
+      case SLACK:
+        return SlackNotification.class;
+      case TWILIO:
+        return TwilioNotification.class;
+      case GITHUB_STATUS:
+        return GithubStatusNotification.class;
+      default:
+        throw new IllegalArgumentException(
+            "No notification type with name \"" + notificationName + "\" handled by halyard");
     }
   }
 }

halyard-cli/src/main/java/com/netflix/spinnaker/halyard/cli/command/v1/config/NotificationCommand.java
@@ -19,6 +19,7 @@
 package com.netflix.spinnaker.halyard.cli.command.v1.config;
 
 import com.beust.jcommander.Parameters;
+import com.netflix.spinnaker.halyard.cli.command.v1.config.notifications.github.GithubStatusCommand;
 import com.netflix.spinnaker.halyard.cli.command.v1.config.notifications.pubsub.PubsubCommand;
 import com.netflix.spinnaker.halyard.cli.command.v1.config.notifications.slack.SlackCommand;
 import com.netflix.spinnaker.halyard.cli.command.v1.config.notifications.twilio.TwilioCommand;
@@ -38,6 +39,7 @@ public class NotificationCommand extends AbstractConfigCommand {
   private String shortDescription = "Display the state of Spinnaker's notification settings.";
 
   public NotificationCommand() {
+    registerSubcommand(new GithubStatusCommand());
     registerSubcommand(new PubsubCommand());
     registerSubcommand(new SlackCommand());
     registerSubcommand(new TwilioCommand());

halyard-cli/src/main/java/com/netflix/spinnaker/halyard/cli/command/v1/config/notifications/github/EditGithubStatusCommand.java
@@ -1,0 +1,41 @@
+/*
+ * Copyright 2019 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.halyard.cli.command.v1.config.notifications.github;
+
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
+import com.netflix.spinnaker.halyard.cli.command.v1.config.notifications.AbstractEditNotificationCommand;
+import com.netflix.spinnaker.halyard.config.model.v1.node.Notification;
+import com.netflix.spinnaker.halyard.config.model.v1.notifications.GithubStatusNotification;
+
+/** Interact with the github notification */
+@Parameters(separators = "=")
+public class EditGithubStatusCommand
+    extends AbstractEditNotificationCommand<GithubStatusNotification> {
+  protected String getNotificationName() {
+    return "github-status";
+  }
+
+  @Parameter(names = "--token", password = true, description = "Your github account token.")
+  private String token;
+
+  @Override
+  protected Notification editNotification(GithubStatusNotification notification) {
+    notification.setToken(isSet(token) ? token : notification.getToken());
+    return notification;
+  }
+}

halyard-cli/src/main/java/com/netflix/spinnaker/halyard/cli/command/v1/config/notifications/github/GithubStatusCommand.java
@@ -1,0 +1,33 @@
+/*
+ * Copyright 2019 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.halyard.cli.command.v1.config.notifications.github;
+
+import com.beust.jcommander.Parameters;
+import com.netflix.spinnaker.halyard.cli.command.v1.config.notifications.AbstractNamedNotificationCommand;
+
+/** Interact with the github notification */
+@Parameters(separators = "=")
+public class GithubStatusCommand extends AbstractNamedNotificationCommand {
+  protected String getNotificationName() {
+    return "github-status";
+  }
+
+  public GithubStatusCommand() {
+    super();
+    registerSubcommand(new EditGithubStatusCommand());
+  }
+}

halyard-config/src/main/java/com/netflix/spinnaker/halyard/config/model/v1/notifications/GithubStatusNotification.java
@@ -1,0 +1,35 @@
+/*
+ * Copyright 2019 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.halyard.config.model.v1.notifications;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.netflix.spinnaker.halyard.config.model.v1.node.Notification;
+import com.netflix.spinnaker.halyard.config.model.v1.node.Secret;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class GithubStatusNotification extends Notification {
+  @Secret String token;
+
+  @Override
+  @JsonIgnore
+  public NotificationType getNotificationType() {
+    return NotificationType.GITHUB_STATUS;
+  }
+}

halyard-config/src/main/java/com/netflix/spinnaker/halyard/config/services/v1/NotificationService.java
@@ -24,6 +24,7 @@ import com.netflix.spinnaker.halyard.config.model.v1.node.DeploymentConfiguratio
 import com.netflix.spinnaker.halyard.config.model.v1.node.NodeFilter;
 import com.netflix.spinnaker.halyard.config.model.v1.node.Notification;
 import com.netflix.spinnaker.halyard.config.model.v1.node.Notifications;
+import com.netflix.spinnaker.halyard.config.model.v1.notifications.GithubStatusNotification;
 import com.netflix.spinnaker.halyard.config.model.v1.notifications.SlackNotification;
 import com.netflix.spinnaker.halyard.config.model.v1.notifications.TwilioNotification;
 import com.netflix.spinnaker.halyard.config.problem.v1.ConfigProblemBuilder;
@@ -91,6 +92,9 @@ public class NotificationService {
         break;
       case TWILIO:
         notifications.setTwilio((TwilioNotification) notification);
+        break;
+      case GITHUB_STATUS:
+        notifications.setGithubStatus((GithubStatusNotification) notification);
         break;
       default:
         throw new IllegalArgumentException(

halyard-deploy/src/main/java/com/netflix/spinnaker/halyard/deploy/spinnaker/v1/profile/deck/DeckProfileFactory.java
@@ -21,6 +21,7 @@ import com.netflix.spinnaker.halyard.config.model.v1.canary.Canary;
 import com.netflix.spinnaker.halyard.config.model.v1.node.DeploymentConfiguration;
 import com.netflix.spinnaker.halyard.config.model.v1.node.Features;
 import com.netflix.spinnaker.halyard.config.model.v1.node.Notifications;
+import com.netflix.spinnaker.halyard.config.model.v1.notifications.GithubStatusNotification;
 import com.netflix.spinnaker.halyard.config.model.v1.notifications.SlackNotification;
 import com.netflix.spinnaker.halyard.config.model.v1.notifications.TwilioNotification;
 import com.netflix.spinnaker.halyard.config.model.v1.providers.appengine.AppengineProvider;
@@ -207,6 +208,9 @@ public class DeckProfileFactory extends RegistryBackedProfileFactory {
 
     TwilioNotification twilioNotification = notifications.getTwilio();
     bindings.put("notifications.twilio.enabled", twilioNotification.isEnabled() + "");
+
+    GithubStatusNotification githubStatusNotification = notifications.getGithubStatus();
+    bindings.put("notifications.github-status.enabled", githubStatusNotification.isEnabled() + "");
 
     // Configure canary
     Canary canary = deploymentConfiguration.getCanary();

spinnaker/halyard

commit ee6382989caca2869f4ba713adcfb743f33e09ed

fix(notifications): fix editing notifications in halyard
Broken in #1374

Closes spinnaker/spinnaker#4834

halyard-config/src/main/java/com/netflix/spinnaker/halyard/config/model/v1/node/Notification.java
@@ -52,6 +52,10 @@ public abstract class Notification extends Node implements Cloneable {
       this.name = name;
     }
 
+    public String getName() {
+      return name;
+    }
+
     @Override
     public String toString() {
       return name;

halyard-config/src/main/java/com/netflix/spinnaker/halyard/config/model/v1/node/Notifications.java
@@ -18,11 +18,16 @@
 
 package com.netflix.spinnaker.halyard.config.model.v1.node;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.netflix.spinnaker.halyard.config.model.v1.node.Notification.NotificationType;
 import com.netflix.spinnaker.halyard.config.model.v1.notifications.GithubStatusNotification;
 import com.netflix.spinnaker.halyard.config.model.v1.notifications.SlackNotification;
 import com.netflix.spinnaker.halyard.config.model.v1.notifications.TwilioNotification;
+import java.util.Optional;
+import java.util.stream.Stream;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -46,8 +51,18 @@ public class Notifications extends Node implements Cloneable {
   }
 
   public static Class<? extends Notification> translateNotificationType(String notificationName) {
-    Class<? extends Notification> n;
-    switch (Notification.NotificationType.valueOf(notificationName)) {
+
+    Optional<NotificationType> notificationType =
+        Stream.of(NotificationType.values())
+            .filter(type -> type.getName().equals(notificationName))
+            .findAny();
+
+    checkArgument(
+        notificationType.isPresent(),
+        "No notification type with name %s handled by halyard",
+        notificationName);
+
+    switch (notificationType.get()) {
       case SLACK:
         return SlackNotification.class;
       case TWILIO: