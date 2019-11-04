3c4d028b4ad49526eca1c7c59a8be9b0b61ae3fe
zengchen1024 <chenzeng2@huawei.com>
2019-11-04 17:47:28 +0800

    feat(huaweicloud): add commands of account
        r 3e29b2c feat(huaweicloud): first commit for huaweicloud
        r 14f49c4 feat(huaweicloud): add commands about account

--- halyard-config/src/main/java/com/netflix/spinnaker/halyard/config/model/v1/providers/huaweicloud/HuaweiCloudAccount.java
+++ halyard-config/src/main/java/com/netflix/spinnaker/halyard/config/model/v1/providers/huaweicloud/HuaweiCloudAccount.java
@@ -1,9 +1,21 @@
 package com.netflix.spinnaker.halyard.config.model.v1.providers.huaweicloud;
 
 import com.netflix.spinnaker.halyard.config.model.v1.node.Account;
+import com.netflix.spinnaker.halyard.config.model.v1.node.Secret;
+import java.util.List;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
-public class HuaweiCloudAccount extends Account {}
+public class HuaweiCloudAccount extends Account {
+  private String accountName;
+  private String accountType;
+  private String authUrl;
+  private String username;
+  @Secret private String password;
+  private String projectName;
+  private String domainName;
+  private Boolean insecure = false;
+  private List<String> regions;
+}
