10285bcafff277842343fdcd12c3060414f3332c
ivanphdz <ivanphdz13@gmail.com>
2020-09-30 16:40:22 -0500

    feat(aws): adding externalId command and field for aws account config

--- halyard-cli/src/main/java/com/netflix/spinnaker/halyard/cli/command/v1/config/providers/aws/AwsCommandProperties.java
+++ halyard-cli/src/main/java/com/netflix/spinnaker/halyard/cli/command/v1/config/providers/aws/AwsCommandProperties.java
@@ -45,6 +45,10 @@ public class AwsCommandProperties {
           + "Security Token Service to assume the specified role.\n\n"
           + "Example: \"user/spinnaker\" or \"role/spinnakerManaged\"";
 
+  public static final String EXTERNAL_ID_DESCRIPTION =
+      "Address and prevent the \"confused deputy\" problem. "
+          + "Provides a way for the account owner to permit the role to be assumed only under specific circumstances.";
+
   public static final String ACCESS_KEY_ID_DESCRIPTION =
       "Your AWS Access Key ID. If not provided, Halyard/Spinnaker will try to find AWS credentials "
           + "as described at http://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/credentials.html#credentials-default";
